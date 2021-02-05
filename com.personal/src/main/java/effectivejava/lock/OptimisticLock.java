da23e8df33024b91d9bcb5151b5e465af77bfe24
wanglong16 <wanglong16@meicai.cn>
2021-02-05 20:51:14 +0800

    lock

---
+++ com.personal/src/main/java/effectivejava/lock/OptimisticLock.java
@@ -0,0 +1,13 @@
+package effectivejava.lock;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @date: 2021-02-04 17:46:25
+ * @author: wanglong16@meicai.cn
+ */
+public class OptimisticLock {
+
+
+
+}
---
+++ com.personal/src/main/java/effectivejava/lock/PessimismLock.java
@@ -0,0 +1,12 @@
+package effectivejava.lock;
+
+/**
+ * @description:
+ * @version: 1.0
+ * @date: 2021-02-04 17:46:49
+ * @author: wanglong16@meicai.cn
+ */
+public class PessimismLock {
+
+    
+}
